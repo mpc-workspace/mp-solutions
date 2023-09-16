6db20380acc63d59b581e01feb259f87db53338f
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-09-16 20:41:14 +0800

    Updates

---
+++ assets/index.043ccbd5.js
@@ -0,0 +1,1 @@
+import{r as u,j as c,u as l,k as f,l as p}from"./entry.8353091d.js";function d(t,a){const e=u({});return c(()=>{const s=f(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:p(r)}}),l(e,a)}export{d as u};
