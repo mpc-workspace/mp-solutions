358d8ae49dbdf72405a7a50db12e80e1acdb8cae
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-09-27 14:21:10 +0800

    Updates

--- assets/index.aa317a68.js
+++
@@ -1,1 +0,0 @@
-import{r as u,j as c,u as l,k as f,l as p}from"./entry.13ba8a89.js";function d(t,a){const e=u({});return c(()=>{const s=f(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:p(r)}}),l(e,a)}export{d as u};
