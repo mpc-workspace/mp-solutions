145a4d99688fbf5a552bafa7b0b1a200b708d7f9
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-24 22:16:19 +0800

    Updates

---
+++ assets/index.df3f5d1b.js
@@ -0,0 +1,1 @@
+import{r as u,l as c,u as l,m as f,q as m}from"./entry.3f149445.js";function d(t,a){const e=u({});return c(()=>{const s=f(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:m(r)}}),l(e,a)}export{d as u};
