59de36d7efff05ed782862a3832baada07a95a75
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-24 21:59:56 +0800

    Updates

--- assets/index.fe150d80.js
+++
@@ -1,1 +0,0 @@
-import{r as u,l as c,u as l,m as f,q as m}from"./entry.568ddf0b.js";function d(t,a){const e=u({});return c(()=>{const s=f(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:m(r)}}),l(e,a)}export{d as u};
